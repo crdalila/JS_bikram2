a713f5f8dc1abe7a0f36527e60079d2eb0ddcd66
Dalila <crdalila@gmail.com>
2025-03-03 13:08:33 +0100

    final push

--- index.js
+++ index.js
@@ -57,5 +57,84 @@ function pintarArray (arr) {
 }
 
 function arrayMapi (arr, funct) {
-    return arr.forEach (funct);
+    return arr.map(funct);
 }
+
+function eliminarDuplicados (arr) {
+    let newArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (!newArray.includes(arr[i])) {
+            newArray.push(arr[i]);
+        }
+    }
+    return newArray;
+}
+
+
+// ARRAYS 2
+
+const arrayNumerosNeg = [0, -1, -2, -3, -4, -5, -6, -7, -8, -9];
+const holaMundo = ['Hola', 'mundo'];
+const loGuardaTodo = ['hola', 'que', 23, 42.33, 'tal'];
+const arrayDeArrays = [[756, 'nombre'], [225, 'apellido'], [298, 'direccion']];
+
+
+// FUNCIONES 2
+
+function multiplicacion (a, b) {
+    return a*b;
+}
+
+function division (a, b) {
+    return a/b;
+}
+
+function esPar (number) {
+    if (number % 2 === 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function resta (a, b) {
+    return a-b;
+}
+
+let arrayFunciones = [suma, resta, multiplicacion];
+
+
+// MEZCLA DE ARRAYS Y FUNCIONES 2
+
+function ordenarArray2 (arr) {
+    return arr.sort(function(a, b) {
+        return b - a; //de mayor a menor, si no sería a - b
+    });
+}
+
+function obtenerImpares(arr) {
+    let arrImpares = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 !== 0) {
+            arrImpares.push(arr[i]);
+        }
+    }
+    return arrImpares;
+}
+
+function sumarArray(arr) {
+    let result = arr.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+    });
+    return result;
+}
+
+function multiplicarArray(arr) {
+    let result = arr.reduce((accumulator, currentValue) => {
+        return accumulator * currentValue;
+    });
+    return result;
+}
+
+console.log(multiplicarArray([2, 24]));
